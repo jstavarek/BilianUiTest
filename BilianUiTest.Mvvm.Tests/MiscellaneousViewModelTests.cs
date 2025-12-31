3632126c49405e01c52eb338ce92f4243ce125fb
Honza <jan.stavarek@email.cz>
2025-12-31 12:26:24 +0100

    Bilian updated

--- BilianUiTest.Mvvm.Tests/MiscellaneousViewModelTests.cs
+++ BilianUiTest.Mvvm.Tests/MiscellaneousViewModelTests.cs
@@ -7,7 +7,7 @@ namespace BilianUiTest.Mvvm.Tests;
 public sealed class MiscellaneousViewModelTests
 {
     [TestMethod]
-    public void Text_is_processed()
+    public void View_model_initialized()
     {
         var viewModel = new MiscellaneousViewModel(new TextProcessor());
         var bench = new ViewModelTestbench(viewModel);
@@ -15,12 +15,18 @@ public sealed class MiscellaneousViewModelTests
 
         Assert.IsNull(viewModel.InputText);
         Assert.IsNull(viewModel.Result);
+    }
+
+    [TestMethod]
+    public void Text_is_processed()
+    {
+        var viewModel = new MiscellaneousViewModel(new TextProcessor());
+        var bench = new ViewModelTestbench(viewModel);
+        bench.Activate();
 
         viewModel.InputText = "text1";
-        Assert.AreEqual("text1", viewModel.InputText);
-        Assert.IsNull(viewModel.Result);
-
         viewModel.ProcessTextCommand.Execute();
+
         Assert.AreEqual("TEXT1", viewModel.Result);
     }
 
@@ -34,13 +40,10 @@ public sealed class MiscellaneousViewModelTests
         viewModel.InputText = "abc";
         viewModel.ProcessTextCommand.Execute();
 
-        var processText = () => viewModel.ProcessTextCommand.Execute();
-        var showingConfirmedWarning = bench.CatchUserInteraction(processText) as ShowingConfirmedWarning;
+        var showingConfirmedWarning = bench.UserInteractions.FirstOrDefault() as ShowingConfirmedWarning;
         Assert.IsNotNull(showingConfirmedWarning, $"{nameof(ShowingConfirmedWarning)} expected");
         Assert.AreEqual("Processing text", showingConfirmedWarning.Title);
         Assert.AreEqual("This word cannot be processed", showingConfirmedWarning.Text);
-
-        bench.InvokeUserInteraction(showingConfirmedWarning);
         Assert.AreEqual("banned word", viewModel.Result);
     }
 }
